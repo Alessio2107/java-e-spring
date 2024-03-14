0acbdf17b44c6cd54e58825ca491c14895e126c4
alessiosalzillo <152988228+Alessio2107@users.noreply.github.com>
2024-03-14 17:56:42 +0100

    Add files via upload

---
+++ SistemaPunteggio/src/main/java/com/example/demo/SistemaPunteggioApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPunteggioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPunteggioApplication.class, args);
+	}
+
+}
