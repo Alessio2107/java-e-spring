238620ac81002d6ea156b7c50a6f7b08ddd3c942
alessiosalzillo <152988228+Alessio2107@users.noreply.github.com>
2024-03-14 17:39:03 +0100

    Add files via upload

---
+++ concessionario/src/main/java/com/example/demo/ConcessionarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcessionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcessionarioApplication.class, args);
+	}
+
+}
---
+++ concessionario/src/main/java/com/example/demo/model/Macchina.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+@Data
+public class Macchina {
+	@Id
+	private long id;
+	
+	@
+	
+
+}
