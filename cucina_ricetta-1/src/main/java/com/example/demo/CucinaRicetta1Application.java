ea3377b48997147dd1cf4101ff61c38b1daf78e8
alessiosalzillo <152988228+Alessio2107@users.noreply.github.com>
2024-03-14 17:41:37 +0100

    Add files via upload

---
+++ cucina_ricetta-1/src/main/java/com/example/demo/CucinaRicetta1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucinaRicetta1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucinaRicetta1Application.class, args);
+	}
+
+}
